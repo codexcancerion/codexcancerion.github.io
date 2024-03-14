820fdde572703b5a4a537cdcc60255e96ed4624b
CodexCancerion <melbertmarafo2022@gmail.com>
2024-03-14 17:36:42 +0800

    Update student registration form and table.

--- projects/stud-form/script.js
+++ projects/stud-form/script.js
@@ -1,46 +1,81 @@
+var odd = 1;
+var oddClass = '<tr class="odd-row">'
+var evenClass = '<tr class="even-row">'
 
+function saveToTable() {
+    // Retrieve form values
+    var fname = document.getElementById('fname').value;
+    var mname = document.getElementById('mname').value;
+    var mnameInitial = mname.charAt(0);
+    var lname = document.getElementById('lname').value;
+    var bdate = document.getElementById('bdate').value;
+    var gender = document.getElementsByName('gender')[0].value;
+    var email = document.getElementById('email').value;
+    var contactNumber = document.getElementById('contact-number').value;
+    var program = document.getElementsByName('program')[0].value;
+    var term = document.getElementsByName('term')[0].value;
+    var enrollNextTerm;
 
-// function validateForm() {
-//     var fname = document.forms["student-form"]["fname"].value;
-    
-// }
-
-// function saveToTable() {
-//     // Get the values from the form inputs containing: fname, mname, lname, bdate, gender, email, contact-number, program, term and student-status  
-//     // get the form data values and store them in variables
-//     let firstName = document.getElementById("fname").value;
-//     let middleName = document.getElementById("mname").value;
-//     let lastName = document.getElementById("lname").value;
-//     let gender = document.getElementById("gender").value;
-//     let emailAddress = document.getElementById("email").value;
-//     let phoneNumber = document.getElementById("phone").value;
-//     let dateOfBirth = document.getElementById("bdate").value;
-//     let program = document.getElementById("program").value;
-//     let term = document.getElementById( "term" ).value;  
-  
-
-//         // Create new row
-//         var table = document.getElementById("studentsTable");
-//         var rowCount = table.rows.length;
-//         var tbody = table.getElementsByTagName("tbody");
-//         var row = tbody.createCell();
-//         if (rowCount < 2) {
-//             var rowIndex = 1;
-//         } else {
-//             var rowIndex = rowCount -  1;
-//         }
+    if(term==1) enrollNextTerm = "&#x2705; Yes";
+    else enrollNextTerm = "&#10062; No";
         
-//         var newRow = table.insertRow(rowIndex);
-      
-//         // Insert cells into the row.
-//         for(var i=0 ;i<6;i++) { 
+    var checkOdd = () => {
+        if(odd==1) {
+            odd=2;
+            return oddClass;
+        }
+        else if(odd==2) {
+            odd=1;
+            return evenClass;
+        }
         
-//         // Insert cells for each column 
-//         var cell1 = row.insertCell(0);
-//         var cell2 = row.insertCell(1);
-//         var cell3 = row.insertCell(2);
-//         var cell4 = row.createCell(3);
+    }
+
+    // Create a new row for the table
+    var newRow = checkOdd+`
+                    <td>${lname.toUpperCase()}, ${fname.toUpperCase()} ${mnameInitial.toUpperCase()}. </td>
+                    <td>${gender.toUpperCase()}</td>
+                    <td>${email}</td>
+                    <td>${program}</td>
+                    <td>${enrollNextTerm}</td>
+                </tr>`;
+
+    // Append the new row to the table body
+    document.getElementById('studentsTable').getElementsByTagName('tbody')[0].innerHTML += newRow;
+
+    // Clear the form after submission
+    document.getElementById('student-form').reset();
+}
+
+function saveTempData() {
+    // Retrieve form values
+    var fname = document.getElementById('temp-fname').value;
+    var mname = document.getElementById('temp-mname').value;
+    var mnameInitial = mname.charAt(0);
+    var lname = document.getElementById('temp-lname').value;
+    var gender = document.getElementsByName('temp-gender')[0].value;
+    var email = document.getElementById('temp-email').value;
+    var program = document.getElementsByName('temp-rogram')[0].value;
+    var term = document.getElementsByName('temp-term')[0].value;
+    var enrollNextTerm;
+
+    if(term==1) enrollNextTerm = "&#x2705; Yes";
+    else enrollNextTerm = "&#10062; No";
         
-//         // Add text to the cells
-//         cell1.innerHTML = "test";
-// }
+
+    // Create a new row for the table
+    var newRow = `
+                <tr>
+                    <td>${lname.toUpperCase()}, ${fname.toUpperCase()} ${mnameInitial.toUpperCase()}. </td>
+                    <td>${gender.toUpperCase()}</td>
+                    <td>${email}</td>
+                    <td>${program}</td>
+                    <td>${enrollNextTerm}</td>
+                </tr>`;
+
+    // Append the new row to the table body
+    document.getElementById('studentsTable').getElementsByTagName('tbody')[0].innerHTML += newRow;
+
+    // Clear the form after submission
+    document.getElementById('student-form').reset();
+}
